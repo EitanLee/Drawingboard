77c0507ea26b16afc022803e580c0e95e56ede25
rqlmj <optimistma@outlook.com>
2019-11-21 02:32:12 +0800

    ios10

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-document.documentElement.addEventListener('touchstart', function (event) {
-    if (event.touches.length > 1) {
-      event.preventDefault();
-    }
-  }, false);
+window.οnlοad=function () {  
+    document.addEventListener('touchstart',function (event) {  
+        if(event.touches.length>1){  
+            event.preventDefault();  
+        }  
+    })  
+    var lastTouchEnd=0;  
+    document.addEventListener('touchend',function (event) {  
+        var now=(new Date()).getTime();  
+        if(now-lastTouchEnd<=300){  
+            event.preventDefault();  
+        }  
+        lastTouchEnd=now;  
+    },false)  
+}
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 var startPaint = false;
